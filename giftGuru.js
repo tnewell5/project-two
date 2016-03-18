eb4183d3fb93d5c49a606f59eeacc5769d363747
Tanya Newell <trozova5@gmail.com>
2016-03-18 11:01:11 -0400

    updates

--- giftGuru.js
+++ giftGuru.js
@@ -3,6 +3,7 @@ console.log("js has loaded");
 
 var query = '';
 var queryType = '';
+var userEtsyCategory = "choose-one";
 
 var etsyCategories = ["accessories", "art", "bags_and_purses", "bath_and_beauty", "books_and_zines", "candles",
 "ceramics_and_pottery", "children", "clothing", "dolls_and_miniatures", "crochet", "furniture", "geekery",
@@ -26,22 +27,28 @@ $submitButton.on("click", function() {
 
   // gets user's search input for the Etsy api:
   var $userEtsySearch = $('#user-etsy-input').val();
-  var userEtsyCategory = etsyCategoriesDropdown.value;
-  console.log(userEtsyCategory);
+  userEtsyCategory = etsyCategoriesDropdown.value;
+  console.log("userEtsyCategory: " + userEtsyCategory);
   //console.log($userEtsySearch);
 
 
 
   //create Etsy listings query:
-  query = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + $userEtsySearch + "&api_key=" + ETSY_KEY + "&callback=foo" + "&includes=MainImage"+"&category=" + userEtsyCategory + "&limit=5";
+  if (userEtsyCategory !== "any-category") {
+    query = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + $userEtsySearch + "&api_key=" + ETSY_KEY + "&callback=foo" + "&includes=MainImage"+"&category=" + userEtsyCategory + "&limit=5";
+  }
+  else {
+    console.log('working');
+    query = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + $userEtsySearch + "&api_key=" + ETSY_KEY + "&callback=foo" + "&includes=MainImage" + "&limit=5";
+  }
   queryType = "listings";
   console.log(query);
   ajaxCall(query, queryType);
 
   //create Etsy treasuries query:
   $userEtsySearch = $userEtsySearch.split(' ').shift();
-  query = "https://openapi.etsy.com/v2/treasuries?keywords=%22red%22&api_key=" + ETSY_KEY;
-  query = "https://openapi.etsy.com/v2/treasuries?keywords=" + $userEtsySearch + "&api_key=" + ETSY_KEY;
+  //query = "https://openapi.etsy.com/v2/treasuries?keywords=%22red%22&api_key=" + ETSY_KEY;
+  query = "https://openapi.etsy.com/v2/treasuries.js?keywords=" + $userEtsySearch + "&api_key=" + ETSY_KEY + "&callback=foo";
   queryType = "treasuries";
   console.log(query);
   ajaxCall(query, queryType);
@@ -78,7 +85,11 @@ $submitButton.on("click", function() {
           //console.log("response.results[0].title" + response.results[0].title);
         } // closes if stmt
         else if (queryType === "treasuries") {
-          
+          var source = document.querySelector('#treasuries-template').innerHTML;
+          var template = Handlebars.compile(source);
+          var templateContainer = document.querySelector('#etsy-treasuries-container');
+          var html = template(response);
+          templateContainer.innerHTML = html;
         }
 
 
